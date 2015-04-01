78c6275caa267dc0a5308f7d0c6e0df6b0d731d5
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2015-04-02 00:41:12 +0200

    Tests (using tips from http://www.whitewashing.de/2012/02/25/symfony2_controller_testing.html)

---
+++ Tests/App/AppKernel.php
@@ -0,0 +1,34 @@
+<?php
+// Tests/Controller/App/AppKernel.php
+
+/** @see http://www.whitewashing.de/2012/02/25/symfony2_controller_testing.html */
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            //~ new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            //~ new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AlexisLefebvre\Bundle\AsyncTweetsBundle\AsyncTweetsBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
+        );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
